88d4d4591e5285010e91ac69cdba86f395299797
ArtemShahov <followdmdv@gmail.com>
2021-09-27 02:19:51 +0300

    styles

--- src/components/Examples/Examples.jsx
+++ src/components/Examples/Examples.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import actions from './actions';
 import selector from './selector';
+import styles from './styles.scss';
 
 function Examples(props) {
   const { count, setCount } = props;
   return (
-    <div>
-      <button type="button" onClick={() => setCount(1)}>
+    <div className={styles.wrap}>
+      <button type="button" className={styles.btn} onClick={() => setCount(-1)}>
         {count}
       </button>
-      <button type="button" onClick={() => setCount(-1)}>
+      <button type="button" className={styles.btn} onClick={() => setCount(1)}>
         {count}
       </button>
     </div>
