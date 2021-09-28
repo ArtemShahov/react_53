0c0d9ef2ccf6b8c4ba6731072e6736997fdae075
ArtemShahov <followdmdv@gmail.com>
2021-09-28 21:03:00 +0300

    mui form

--- src/components/pages/KanbanBoardCompanent/NewTaskForm/NewTaskForm.jsx
+++ src/components/pages/KanbanBoardCompanent/NewTaskForm/NewTaskForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/jsx-closing-bracket-location */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
@@ -8,45 +9,77 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import { connect } from 'react-redux';
+import CloseIcon from '@mui/icons-material/Close';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Button from '@mui/material/Button';
+import kanbanConfig from '../../../../config/kanbanBoardsConfig';
+// import Box from '@mui/material/Box';
 import styles from './style.scss';
+import actions from '../KanbanBoard/actions';
 
-function NewTaskForm() {
+function NewTaskForm(props) {
+  const { toggleModal } = props;
+
   return (
-    <Paper elevation={3} sx={{ p: 2 }}>
-      <header className={styles.header}>
-        <Typography variant="h5" sx={{ mb: 2 }}>
-          <div className={styles.closer} />
-          Create new task
-        </Typography>
-      </header>
-      <div className={styles.wrap}>
-        <div className={styles.column}>
+    <div className={styles.wrap}>
+      <Paper elevation={3} sx={{ p: 2 }}>
+        <header className={styles.header}>
+          <Typography variant="h5" sx={{ mb: 2 }}>
+            <button
+              type="button"
+              className={styles.closer}
+              onClick={toggleModal}
+            >
+              <CloseIcon />
+            </button>
+            Create new task
+          </Typography>
+        </header>
+        <div className={styles.fieldSet}>
+          <TextField
+            fullWidth
+            id="filled-basic"
+            label="Title"
+            variant="outlined"
+          />
+          <TextField
+            fullWidth
+            id="filled-basic"
+            label="Status"
+            variant="outlined"
+          />
           <TextField
             fullWidth
             id="filled-basic"
             label="Filled"
             variant="outlined"
           />
-          <TextField
-            fullWidth
-            id="standard-basic"
-            label="Standard"
-            variant="outlined"
-          />
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-helper-label">Status</InputLabel>
+            <Select
+              fullWidth
+              labelId="Status"
+              id="demo-simple-select"
+              label="Status"
+            >
+              {kanbanConfig.map((item) => (
+                <MenuItem key={item.status} value={item.status}>
+                  {item.title}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <div className={styles.btnWrap}>
+            <Button variant="contained">Отправить</Button>
+            <Button>Очистить</Button>
+          </div>
         </div>
-        <div className={styles.column}>
-          <TextField
-            fullWidth
-            fullHeight
-            id="outlined-multiline-flexible"
-            multiline
-            rows={4}
-            label="Outlined"
-            variant="outlined"
-          />
-        </div>
-      </div>
-    </Paper>
+      </Paper>
+    </div>
   );
 }
 
@@ -55,4 +88,26 @@ function NewTaskForm() {
 //   // tasks: PropTypes.arrayOf.isRequired,
 // };
 
-export default NewTaskForm;
+export default connect(null, { ...actions })(NewTaskForm);
+
+/* <TextField
+fullWidth
+id="filled-basic"
+label="Filled"
+variant="outlined"
+/>
+<TextField
+fullWidth
+id="standard-basic"
+label="Standard"
+variant="outlined"
+/>
+<TextField
+fullWidth
+fullHeight
+id="outlined-multiline-flexible"
+multiline
+rows={4}
+label="Outlined"
+variant="outlined"
+/> */
